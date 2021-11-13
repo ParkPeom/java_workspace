f4f7b1e65c2130c3905800dcc76b36888139b3e4
parkpeom94@gmail.com <parkpeom94@gmail.com>
2021-11-13 23:37:06 +0900

    클라이언트 소켓 과 서버소켓 연동 스트림 입출력

--- Java_Basic_Grammer/src/com/codechobo/network/ServerSocketTest.java
+++ Java_Basic_Grammer/src/com/codechobo/network/ServerSocketTest.java
@@ -1,32 +1,47 @@
 package com.codechobo.network;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+// 1. 서버
 public class ServerSocketTest {
 
 	public ServerSocketTest() {
-		//접속을 대기하는 ServerSocket객체를 생성한다.
+		// 2 . 접속을 대기하는 ServerSocket객체를 생성한다.
 		try {
 			ServerSocket ss = new ServerSocket(12000);
-			System.out.println("접속대기중.....");
+		
+		while(true) {
+			// 3. 서버 접속 대기
+			System.out.println("접속대기중.....");	
+			Socket s = ss.accept(); 
 			
-			Socket s = ss.accept();
-			
-			//접속자의 ip얻어오기
+			// 6. 접속자의 ip얻어오기
 			InetAddress ia = s.getInetAddress();//접속자 컴퓨터의 ip
 			System.out.println(ia.getHostAddress()+"-->클라이언트 접속했습니다.");
 			
-			
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-			bw.write("안녕 나 서버야...");
-			bw.flush();
-			bw.close();
-			
-			
+		
+			// 7. 서버가 클라이언트에 메시지 보내기
+			OutputStreamWriter osw = new OutputStreamWriter(s.getOutputStream());
+			PrintWriter pw = new PrintWriter(osw);
+			pw.println("너 아이피 " + ia.getHostAddress() +"이지 ?? ");
+//			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+//			bw.write("안녕 나 서버야...\n");
+//			pw.flush();
+			pw.flush();
+			// 10.
+			// 클라이언트가 보낸 문자를 받기 위한 inputStream 객체가 있어야 한다.
+			// 한줄로  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 InputStreamReader isr = new InputStreamReader(s.getInputStream());
+			 BufferedReader br = new BufferedReader(isr);
+			 String sendData = br.readLine(); // 클라이언트가 보낸 문자를 받음
+			 System.out.println("서버가 받은 문자----->"+sendData);
+			}
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
--- Java_Basic_Grammer/src/com/codechobo/network/SocketTest.java
+++ Java_Basic_Grammer/src/com/codechobo/network/SocketTest.java
@@ -2,20 +2,33 @@ package com.codechobo.network;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
+// 클라이언트 
 public class SocketTest {
 
 	public SocketTest() {
 		try {
-			//서버에 접속하기 10.10.4.103
-			InetAddress ia = InetAddress.getByName("10.10.4.103");
-			//서버에 접속함
+			// 4. 서버에 접속하기 172.30.34.113
+			InetAddress ia = InetAddress.getByName("172.30.34.113");
+			// 5. 서버 12000에 접속함
 			Socket s = new Socket(ia, 12000);
+			
+			// 8.  서버로부터 받음 
 			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			String receive = br.readLine();
 			System.out.println(receive);
+			
+			// 9. 클라이언트가 서버로 데이터 보내기 - 문자 단위로 보냄 
+			OutputStreamWriter osw = new OutputStreamWriter(s.getOutputStream());
+			PrintWriter pw = new PrintWriter(osw);
+			// 서버로 문자쓰기
+			pw.println("하이");
+			pw.flush();
+			pw.close();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
