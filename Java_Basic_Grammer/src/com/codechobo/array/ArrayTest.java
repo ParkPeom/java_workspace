696aec7fd1f7cc920eb36e5bd8d4b69d91af7964
parkpeom94@gmail.com <parkpeom94@gmail.com>
2021-11-15 00:06:45 +0900

    Arrays Test

---
+++ Java_Basic_Grammer/src/com/codechobo/array/ArrayTest.java
@@ -0,0 +1,21 @@
+package com.codechobo.array;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+	
+		HashMap<String,Integer> hashmap = new HashMap();
+		hashmap.put("1", 1);
+		hashmap.put("2", 2);
+		hashmap.put("3", 3);
+		hashmap.put("4", 4);
+		hashmap.put("3", 3);
+		
+		
+	}
+}
