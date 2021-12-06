dcbb7fb78b876d47ef39db86e83d41615ead298b
parkpeom94 <parkpeom94@gmail.com>
2021-12-06 21:36:45 +0900

    학생관리시스템

---
+++ ITwill_jdbc/src/xyz/itwill/student/JdbcDAO.java
@@ -0,0 +1,67 @@
+package xyz.itwill.student;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
+
+//DBCP(DataBase Connection Pool) 관련 인스턴스를 생성하여 Connection 인스턴스를 제공받아
+//반환하거나 JDBC 관련 인스턴스를 전달받아 제거하는 기능을 제공하는 클래스
+// => DBMS 관련 DAO 클래스에서 상속받아 사용하는 클래스 
+// => 추상클래스로 작성하는 것을 권장 - 상속을 목적으로 작성된 클래스 
+public abstract class JdbcDAO {
+	
+	private static PoolDataSource _pds;
+	
+	static { // PoolDataSource 인스턴스를 생성하여 시스템 필드에 저장 - DBCP
+		
+		_pds = PoolDataSourceFactory.getPoolDataSource();
+					// PoolDataSourceFactory 인스턴스를 만들어주는 공장같은 클래스
+					// PoolDataSource 객체를 딱 하나만 만들어준다. 
+					// 메모리에 하나만 만들어서 하나로만 쓴다. 
+		try {
+			_pds.setConnectionFactoryClassName("oracle.jdbc.driver.OracleDriver");
+			_pds.setURL("jdbc:oracle:thin:@localhost:1522:orcl");
+			_pds.setUser("scott");
+			_pds.setPassword("tiger");
+			_pds.setInitialPoolSize(3); // 최소 사이즈
+			_pds.setMaxPoolSize(5); // 최대 
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection() {
+		Connection con = null;
+		try {
+			con = _pds.getConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public void close(Connection con , PreparedStatement pstmt , ResultSet rs) {
+		try {
+			if(con!=null) con.close();
+			if(pstmt!=null) pstmt.close();
+			if(rs!=null) rs.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void close(Connection con , PreparedStatement pstmt) {
+		try {
+			if(con!=null) con.close();
+			if(pstmt!=null) pstmt.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ ITwill_jdbc/src/xyz/itwill/student/StudentGUIApp.java
@@ -0,0 +1,248 @@
+package xyz.itwill.student;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+public class StudentGUIApp extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1L;
+
+	public static final int NONE = 0;
+	public static final int ADD = 1;
+	public static final int DELETE = 2;
+	public static final int UPDATE = 3;
+	public static final int UPDATE_CHANGE = 4;
+	public static final int SEARCH = 5;
+
+	JTextField noTF, nameTF, phoneTF, addressTF, birthdayTF;
+	JButton addB, deleteB, updateB, searchB, cancelB;
+	
+	JTable table;
+	
+	int cmd;
+	/********************************************
+	 * 생성자 정의
+	 *********************************************/
+	public StudentGUIApp() throws Exception {
+		setTitle("◆◆◆ 학생 관리 프로그램 ◆◆◆");
+		setSize(800, 400);
+
+		Dimension dim = getToolkit().getScreenSize();
+		setLocation(dim.width / 2 - getWidth() / 2, dim.height / 2
+				- getHeight() / 2);
+
+		JPanel left = new JPanel();
+		left.setLayout(new GridLayout(5, 1));
+
+		JPanel pno = new JPanel();
+		pno.add(new JLabel("번  호"));
+		pno.add(noTF = new JTextField(10));
+
+		JPanel pname = new JPanel();
+		pname.add(new JLabel("이  름"));
+		pname.add(nameTF = new JTextField(10));
+		
+		JPanel pbirthday = new JPanel();
+		pbirthday.add(new JLabel("생  일"));
+		pbirthday.add(birthdayTF = new JTextField(10));
+		
+		JPanel pphone = new JPanel();
+		pphone.add(new JLabel("전  화"));
+		pphone.add(phoneTF = new JTextField(10));
+
+		JPanel paddress = new JPanel();
+		paddress.add(new JLabel("주  소"));
+		paddress.add(addressTF = new JTextField(10));
+
+		left.add(pno);
+		left.add(pname);
+		left.add(pphone);
+		left.add(paddress);
+		left.add(pbirthday);
+		
+		JPanel bottom = new JPanel();
+		bottom.setLayout(new GridLayout(1, 5));
+
+		bottom.add(addB = new JButton("추  가"));
+		addB.addActionListener(this);
+
+		bottom.add(deleteB = new JButton("삭  제"));
+		deleteB.addActionListener(this);
+		
+		bottom.add(updateB = new JButton("변  경"));
+		updateB.addActionListener(this);
+
+		bottom.add(searchB = new JButton("검  색"));
+		searchB.addActionListener(this);
+		
+		bottom.add(cancelB = new JButton("초기화"));
+		cancelB.addActionListener(this);
+
+		Object[] title={"학번","이름","전화번호","주소","생년월일"};
+		table=new JTable(new DefaultTableModel(title, 0));
+		table.setEnabled(false);
+		table.getTableHeader().setReorderingAllowed(false);
+		table.getTableHeader().setResizingAllowed(false);
+
+		JScrollPane sp=new JScrollPane(table);
+		
+		add(sp, "Center");
+		add(left, "West");
+		add(bottom, "South");
+		cmd = NONE;
+		initialize();
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public void initialize() {
+		noTF.setEditable(false);
+		nameTF.setEditable(false);
+		phoneTF.setEditable(false);
+		addressTF.setEditable(false);
+		birthdayTF.setEditable(false);
+	}
+
+	public void setEditable(int n) {
+		switch (n) {
+		case ADD:
+			noTF.setEditable(true);
+			nameTF.setEditable(true);
+			phoneTF.setEditable(true);
+			addressTF.setEditable(true);
+			birthdayTF.setEditable(true);
+			break;
+		case DELETE:
+			noTF.setEditable(true);
+			break;
+		case UPDATE:
+			noTF.setEditable(true);
+			break;
+		case UPDATE_CHANGE:
+			noTF.setEditable(false);
+			nameTF.setEditable(true);
+			phoneTF.setEditable(true);
+			addressTF.setEditable(true);
+			birthdayTF.setEditable(true);
+			break;
+		case SEARCH:
+			nameTF.setEditable(true);
+			break;
+		case NONE:
+			noTF.setEditable(false);
+			nameTF.setEditable(false);
+			phoneTF.setEditable(false);
+			addressTF.setEditable(false);
+			birthdayTF.setEditable(false);
+		}
+	}
+
+	public void setEnable(int n) {
+		addB.setEnabled(false);
+		deleteB.setEnabled(false);
+		updateB.setEnabled(false);
+		searchB.setEnabled(false);
+
+		switch (n) {
+		case ADD:
+			addB.setEnabled(true);
+			setEditable(ADD);
+			cmd = ADD;
+			break;
+		case DELETE:
+			deleteB.setEnabled(true);
+			setEditable(DELETE);
+			cmd = DELETE;
+			break;
+		case UPDATE:
+			updateB.setEnabled(true);
+			setEditable(UPDATE);
+			cmd = UPDATE;
+			break;			
+		case UPDATE_CHANGE:
+			updateB.setEnabled(true);
+			setEditable(UPDATE_CHANGE);
+			cmd = UPDATE_CHANGE;
+			break;			
+		case SEARCH:
+			searchB.setEnabled(true);
+			setEditable(SEARCH);
+			cmd = SEARCH;
+			break;
+		case NONE:
+			addB.setEnabled(true);
+			deleteB.setEnabled(true);
+			updateB.setEnabled(true);
+			searchB.setEnabled(true);
+		}
+	}
+
+	public void clear() {
+		noTF.setText("");
+		nameTF.setText("");
+		phoneTF.setText("");
+		addressTF.setText("");
+		birthdayTF.setText("");
+	}
+
+	public void initDisplay() {
+		setEnable(NONE);
+		clear();
+		cmd = NONE;
+		initialize();		
+	}
+
+	public static void main(String args[]) throws Exception {
+		new StudentGUIApp();
+	}
+	
+	public void actionPerformed(ActionEvent ev) {
+		Component c = (Component) ev.getSource();
+		try {
+			if (c == addB) {//추가 버튼을 누른 경우
+				if (cmd != ADD) {//ADD 상태가 아닌 경우
+					setEnable(ADD);//ADD 상태로 활성화					
+				} else {//ADD 상태인 경우
+					initDisplay();	
+				}
+			} else if (c == deleteB) {
+				if (cmd != DELETE) {
+					setEnable(DELETE);
+				} else {
+					initDisplay();				
+				}
+			} else if (c == updateB) {
+				if (cmd != UPDATE && cmd != UPDATE_CHANGE) {
+					setEnable(UPDATE);
+				} else if (cmd != UPDATE_CHANGE) {
+					setEnable(UPDATE_CHANGE);
+				} else {
+					initDisplay();
+				}
+			} else if (c == searchB) {
+				if (cmd != SEARCH) {
+					setEnable(SEARCH);
+				} else {
+					initDisplay();	
+				}
+			} else if (c == cancelB) {
+				initDisplay();
+			}
+		} catch (Exception e) {
+			System.out.println("예외 발생 : " + e);
+		}		
+	}
+
+}
