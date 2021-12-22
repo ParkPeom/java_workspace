0e1d8bdb1537ca4f7de7679522d125fc73bcde28
parkpeom94 <parkpeom94@naver.com>
2021-12-22 17:03:42 +0900

    singleton pattern

---
+++ 싱글톤패턴2/src/싱글톤패턴2/Person.java
@@ -0,0 +1,14 @@
+package ΩÃ±€≈Ê∆–≈œ2;
+
+public class Person {
+	// ¿ŒΩ∫≈œΩ∫¿« ∞πºˆ ¡¶æÓ -> ΩÃ±€≈Ê ∆–≈œ 
+	private final static Person p = new Person();
+
+	private Person() {}
+
+	public static Person getInstance() {
+		return p;
+	}
+}
+
+
---
+++ 싱글톤패턴2/src/싱글톤패턴2/PersonApp.java
@@ -0,0 +1,10 @@
+package 싱글톤패턴2;
+
+public class PersonApp {
+	
+	public static void main(String[] args) {
+		// 싱글톤 패턴 : 이미 존재하고 있는 객체 리턴
+		Person p = Person.getInstance();
+		
+	}
+}
